7cd1eb00501599bdbce61f82ba63b6009237aa8b
Rohit <parabrohit161@gmail.com>
2025-03-09 22:51:30 +0530

    Duplicate & Clean slate

--- assets/js/Task.js
+++ assets/js/Task.js
@@ -3,32 +3,41 @@ class Task{
     incomplete = "";
     complete = "";
     task = "";
-    sampleTask = "";
+    sampleTask= "";
     status = 0;
 
     constructor(){
     }
 
     createTask = () => {
+
         const new_task = this.sampleTask[0].cloneNode(true);
-        const total = this.getTotalTasks();
 
-        new_task.setAttribute("data-status",0);
-        new_task.setAttribute("data-task-no",total + 1);
-
-        new_task.removeAttribute("id");
-        this.incomplete[0].append(new_task);
-
+        if(!this.checkDuplicate(new_task.firstElementChild.value)){
+            const total = this.getTotalTasks();
+            new_task.setAttribute("data-status",0);
+            new_task.setAttribute("data-task-no",total + 1);
+    
+            new_task.removeAttribute("id");
+            this.incomplete[0].append(new_task);
+    
+            this.cleanSlate();
+        }
     }
 
     getTotalTasks = () => document.querySelectorAll('[data-status="0"]').length;
 
     checkDuplicate = (task_name) => {
         let flag = 0;
-      
+        Array.from(this.incomplete[0].children).forEach(element => {
+            if(element.firstElementChild.value == task_name && element.id == ""){
+                flag = 1;
+            }
+        });
         return flag;
     }
 
+    cleanSlate = () => this.sampleTask[0].firstElementChild.value = ""
 }
 
 class IncompleteTask extends Task{
