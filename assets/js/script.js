72b1d7919f942b9e5a9faba0cb732e54b48c42ad
Rohit <parabrohit161@gmail.com>
2025-03-09 17:35:50 +0530

    Skip Empty Tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
     const enterKey = 13;
     const taskList = $("#task-list");
     const tasks = $(".task");
-
+    const sampleTask = $("#sample-task");
     // event triggers...
     $("#sample-task input").on("keypress",(event) => {
-        if(event.which == enterKey){
+        if(event.which == enterKey && event.target.value != ""){
             if(!checkDuplicate(event.target.value)){
                 createTask(event.target.value);
+                cleanSlate();
             }
-
         }
     })
     // event triggers...
 
     createTask = () => {
-        const new_task = $("#sample-task").clone();
+        const new_task = $(sampleTask).clone();
         new_task.removeAttr("id");
         new_task.children("input").addClass("inp");
         new_task.prependTo("#task-list");
@@ -29,4 +29,6 @@ $(document).ready(function(){
         })
         return flag;
     }
+
+    cleanSlate = () => $(sampleTask).children("input").val("");
 })
