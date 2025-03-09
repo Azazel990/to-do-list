8d1b5ce7b61540d54701eed2bd56c9392780289d
Rohit <parabrohit161@gmail.com>
2025-03-09 17:18:49 +0530

    Adding Task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
 $(document).ready(function(){
+    
+    const enterKey = 13;
+    const taskList = $("#task-list");
+    const tasks = $(".task");
+
+    // event triggers...
+    $("#sample-task input").on("keypress",(event) => {
+        if(event.which == enterKey){
+            if(!checkDuplicate(event.target.value)){
+                createTask(event.target.value);
+            }
+
+        }
+    })
+    // event triggers...
+
+    createTask = () => {
+        const new_task = $("#sample-task").clone();
+        new_task.removeAttr("id");
+        new_task.children("input").addClass("inp");
+        new_task.prependTo("#task-list");
+    }
+
+    checkDuplicate = (task) => {
+        let flag = 0;
+        $(".inp").each((key,element) => {
+            if(element.value.toLowerCase() == task.toLowerCase()) flag = 1;
+        })
+        return flag;
+    }
 })
